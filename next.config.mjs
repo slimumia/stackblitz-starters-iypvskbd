c8b66b3f9a69b908ceffa98704492153e20affc6
slimumia <slimumia1101@gmail.com>
2026-08-27 16:38:31 +0800

    強化 sw.ts 絕對路徑設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
-import withSerwistInit from '@serwist/next';
+import withSerwistInit from "@serwist/next";
 
 const withSerwist = withSerwistInit({
-  swSrc: 'src/sw.ts',
-  swDest: 'public/sw.js',
-  disable: process.env.NODE_ENV === 'development', // 建議開發模式先關閉快取避免干擾，測試離線時再開啟
+  swSrc: "sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development", 
 });
 
 export default withSerwist({
   reactStrictMode: true,
-  // 🟢 保持之前設定，強制關閉快取以避開 StackBlitz 檔案系統鎖死問題
   webpack: (config) => {
     config.cache = false;
     return config;
